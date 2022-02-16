efd9e2ddda5ba6fc5e31b7829bf7ae821bc5e2ad
UO277068 <uo277068@uniovi.es>
2022-02-16 11:35:00 +0100

    SDI-2122-101-2.1-CreateSpringProject

---
+++ src/main/java/com/uniovi/sdi2122712spring/Sdi2122712SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122712spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122712SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122712SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2122712spring/Sdi2122712SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122712spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122712SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
