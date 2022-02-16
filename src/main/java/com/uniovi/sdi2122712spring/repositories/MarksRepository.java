869fea2c1a1c20607fed22e57eb2c7eeb22b2fb1
UO277068 <uo277068@uniovi.es>
2022-02-16 19:10:54 +0100

    SDI-2122-101-2.4-Modelo,acceso_a_datos_simple

---
+++ src/main/java/com/uniovi/sdi2122712spring/repositories/MarksRepository.java
@@ -0,0 +1,10 @@
+package com.uniovi.sdi2122712spring.repositories;
+
+import com.uniovi.sdi2122712spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface MarksRepository extends CrudRepository<Mark,Long>
+{
+
+}
