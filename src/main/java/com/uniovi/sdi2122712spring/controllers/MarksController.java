a1d005c9a692dfde044176b2ad92fa0572335311
UO277068 <uo277068@uniovi.es>
2022-02-16 16:33:03 +0100

    SDI-2122-101-2.2-Trabajando_con_Controladores

---
+++ src/main/java/com/uniovi/sdi2122712spring/controllers/MarksController.java
@@ -0,0 +1,30 @@
+package com.uniovi.sdi2122712spring.controllers;
+
+import com.uniovi.sdi2122712spring.entities.Mark;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MarksController {
+
+    @RequestMapping("/mark/list")
+    public String getList(){
+        return "Getting List";
+    }
+
+    /* Version anterior
+    @RequestMapping(value="/mark/add",method= RequestMethod.POST)
+    public String setMark(@RequestParam String description,@RequestParam String score){
+        return "Added:"+description+"with score"+score;
+    }
+     */
+
+    @RequestMapping(value="/mark/add",method= RequestMethod.POST)
+    public String setMark(@ModelAttribute Mark mark){
+        return "Added:"+mark.getDescription()+"with score:"+mark.getScore() + "id:"+mark.getId();
+    }
+
+    @RequestMapping("/mark/details/{id}")
+    public String getDetail(@PathVariable Long id){
+        return "Getting Details=>"+id;
+    }
+}
---
+++ src/main/java/com/uniovi/sdi2122712spring/entities/Mark.java
@@ -0,0 +1,32 @@
+package com.uniovi.sdi2122712spring.entities;
+
+public class Mark {
+
+    private long id;
+    private String description;
+    private double score;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
